eefd125ef92f66790719fdf7214974da7cb0089c
ahilan-subbaian <ahilmails1@gmail.com>
2023-07-16 05:43:18 -0400

    added printing

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from alpaca.trading.requests import MarketOrderRequest, GetCalendarRequest
 from alpaca.trading.enums import OrderSide, TimeInForce
 import datetime
 from tenacity import retry, stop_after_attempt, wait_exponential
-from alpaca.error import APIError
 
 
 CONSTANTS = {
@@ -26,18 +25,17 @@ CONSTANTS = {
 # method to use with tenacity to retry if API call fails
 @retry(stop=stop_after_attempt(3), wait=wait_exponential(multiplier=1, min=4, max=10), reraise=True)
 def submit_order_with_retry(client, order):
-    try:
-        client.submit_order(order)
-    except APIError as e:
-        print(f"APIError occurred: {str(e)}")
-        raise
+    print("Submitting order: ", order)
+    client.submit_order(order)
 
 @retry(stop=stop_after_attempt(3), wait=wait_exponential(multiplier=1, min=4, max=10))
 def get_calendar_with_retry(client, calendar_request):
+    print("Getting calendar: ", calendar_request)
     return client.get_calendar(calendar_request)
 
 @retry(stop=stop_after_attempt(3), wait=wait_exponential(multiplier=1, min=4, max=10))
 def fetch_price_with_retry(ticker, broker):
+    print("Fetching price for: ", ticker)
     return broker.get_stock_latest_quote(
                     StockLatestQuoteRequest(symbol_or_symbols=ticker))[ticker].bid_price
 
