f3368f316f1baf73baec477909d4300511882ed5
ahilan-subbaian <ahilmails1@gmail.com>
2023-09-21 16:57:12 -0400

    int conversion

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ def handler(event, context):
     print("Calendar: ", calendar)
 
     # make sure we have enough cash to make trades
-    cash = client.get_account().cash
+    cash = int(client.get_account().cash)
     if cash < CONSTANTS["AMOUNT"]:
         CONSTANTS["AMOUNT"] = min(CONSTANTS["AMOUNT"], cash - 1)
         logging.error("Not enough cash to make trades. Amount changed to: ", CONSTANTS["AMOUNT"])
