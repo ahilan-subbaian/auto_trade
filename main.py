6921116b992c7b403932d3dbd728c5b9d734a4cf
ahilan-subbaian <ahilmails1@gmail.com>
2023-10-07 02:00:40 -0400

    format

--- main.py
+++ main.py
@@ -1,44 +1,55 @@
 # import libraries
-from dotenv import load_dotenv
-import os
-from alpaca.data.historical import StockHistoricalDataClient
-from alpaca.trading.client import TradingClient
-from alpaca.data.requests import StockLatestQuoteRequest
-from alpaca.trading.requests import MarketOrderRequest, GetCalendarRequest
-from alpaca.trading.enums import OrderSide, TimeInForce
 import datetime
-from tenacity import retry, stop_after_attempt, wait_exponential
 import logging
+import os
 
+from alpaca.data.historical import StockHistoricalDataClient
+from alpaca.data.requests import StockLatestQuoteRequest
+from alpaca.trading.client import TradingClient
+from alpaca.trading.enums import OrderSide, TimeInForce
+from alpaca.trading.requests import GetCalendarRequest, MarketOrderRequest
+from dotenv import load_dotenv
+from tenacity import retry, stop_after_attempt, wait_exponential
 
 CONSTANTS = {
-    "NOTIONAL" : {
-        "VTI": .40,
-        "VXUS": .20,
-        "VGT": .25,
+    "NOTIONAL": {
+        "VTI": 0.40,
+        "VXUS": 0.20,
+        "VGT": 0.25,
     },
-    "QTY" : {
-        "PTY": .15,
+    "QTY": {
+        "PTY": 0.15,
     },
-    "AMOUNT" : 1500,
+    "AMOUNT": 1500,
 }
 
+
 # method to use with tenacity to retry if API call fails
-@retry(stop=stop_after_attempt(3), wait=wait_exponential(multiplier=1, min=4, max=10), reraise=True)
+@retry(
+    stop=stop_after_attempt(3),
+    wait=wait_exponential(multiplier=1, min=4, max=10),
+    reraise=True,
+)
 def submit_order_with_retry(client: TradingClient, order: MarketOrderRequest):
     print("Submitting order: ", order)
     client.submit_order(order)
 
+
 @retry(stop=stop_after_attempt(3), wait=wait_exponential(multiplier=1, min=4, max=10))
-def get_calendar_with_retry(client: TradingClient, calendar_request: GetCalendarRequest):
+def get_calendar_with_retry(
+    client: TradingClient, calendar_request: GetCalendarRequest
+):
     print("Getting calendar: ", calendar_request)
     return client.get_calendar(calendar_request)
+
 
 @retry(stop=stop_after_attempt(3), wait=wait_exponential(multiplier=1, min=4, max=10))
 def fetch_price_with_retry(broker: StockHistoricalDataClient, ticker: str):
     print("Fetching price for: ", ticker)
     return broker.get_stock_latest_quote(
-                    StockLatestQuoteRequest(symbol_or_symbols=ticker))[ticker].bid_price
+        StockLatestQuoteRequest(symbol_or_symbols=ticker)
+    )[ticker].bid_price
+
 
 def handler(event, context):
     # load environment variables based on paper/live trading
@@ -64,7 +75,9 @@ def handler(event, context):
     cash = float(client.get_account().cash)
     if cash < CONSTANTS["AMOUNT"]:
         CONSTANTS["AMOUNT"] = min(CONSTANTS["AMOUNT"], cash - 1)
-        logging.error("Not enough cash to make trades. Amount changed to: ", CONSTANTS["AMOUNT"])
+        logging.error(
+            "Not enough cash to make trades. Amount changed to: ", CONSTANTS["AMOUNT"]
+        )
 
     # if today is the last open day of the week
     if len(calendar) > 0 and calendar[-1].date == start:
@@ -82,7 +95,7 @@ def handler(event, context):
                 )
                 submit_order_with_retry(client, order)
                 print("Order submitted for: ", ticker)
-            
+
             # quantity trades
             for ticker in CONSTANTS["QTY"]:
                 last_price = fetch_price_with_retry(broker, ticker)
